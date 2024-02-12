ed0ef265e8a67848e47293095734e9777776f16e
dannycahyo <dannydwicahyono@gmail.com>
2024-02-12 14:32:52 +0700

    feat: adjust the mock API for fos purposes

--- server.js
+++ server.js
@@ -4,29 +4,25 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router); // Move the router middleware before the custom middleware
 
-// Custom output middleware
-server.use((req, res, next) => {
-  if (
-    res.get("Content-Type") &&
-    res.get("Content-Type").includes("application/json")
-  ) {
-    const oldJson = res.json;
+server.get("/financing/house-type/find-all", (req, res) => {
+  const db = router.db;
+  const housTypeData = db.get("house-type");
 
-    res.json = function (data) {
-      oldJson.call(this, {
-        timestamp: new Date().toISOString(),
-        statusCode: "00",
-        statusDescription: "Success",
-        statusTitle: "OK",
-        data: data,
-      });
-    };
-  }
-
-  next();
+  res.jsonp({
+    data: housTypeData,
+  });
 });
+
+server.use(
+  jsonServer.rewriter({
+    "/fos/*": "/$1",
+    "/financing/*": "/$1",
+  }),
+);
+
+server.use(jsonServer.bodyParser);
+server.use(router);
 
 const port = process.env.PORT || 30001;
 server.listen(port, () => {
