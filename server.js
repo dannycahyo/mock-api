6a32f937f0ba290beb61e1c70c7f1ae3980668b9
dannycahyo <dannydwicahyono@gmail.com>
2024-02-13 09:00:20 +0700

    feat: add synchronize collateral API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 server.get("/financing/house-type/find-all", (req, res) => {
   const db = router.db;
@@ -14,6 +15,21 @@ server.get("/financing/house-type/find-all", (req, res) => {
   });
 });
 
+server.get("/synchronize-collateral", (req, res) => {
+  const { registrationNumber } = req.body;
+  const db = router.db;
+  const sycnhronizeCollateralData = db.get("synchronize-collateral");
+
+  const collateralData = sycnhronizeCollateralData.value();
+
+  res.jsonp({
+    data: {
+      registrationNumber,
+      ...collateralData,
+    },
+  });
+});
+
 server.use(
   jsonServer.rewriter({
     "/fos/*": "/$1",
@@ -21,10 +37,9 @@ server.use(
   }),
 );
 
-server.use(jsonServer.bodyParser);
 server.use(router);
 
-const port = process.env.PORT || 30001;
+const port = process.env.PORT || 3001;
 server.listen(port, () => {
   console.log(`JSON Server is running on port http://localhost:${port}`);
 });
